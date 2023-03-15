a34e3fc68700562d914f751270f2c2ca0dfb9276
Esben Søe <eso@trifork.com>
2023-03-15 21:20:16 +0100

    nowork

--- webserver.py
+++ webserver.py
@@ -7,9 +7,7 @@ app = Flask(__name__)
 def hello():
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
-    result = 0
-    for i in range(1, 10000000):
-        result += i ** 2
+    
     return f"Hello, wafaewfraewfarld! {ip_address}"
 
 if __name__ == '__main__':

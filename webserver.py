7b723cbfa4354908e95e47c0bb5dc2d072c43c3d
Esben Søe <eso@trifork.com>
2023-03-15 21:33:26 +0100

    less work

--- webserver.py
+++ webserver.py
@@ -8,7 +8,7 @@ def hello():
     hostname = socket.gethostname()
     ip_address = socket.gethostbyname(hostname)
     result = 0
-    for i in range(1, 10000000):
+    for i in range(1, 1000000):
         result += i ** 2
     return f"Hello, wafaewfraewfarld! {ip_address}"
 

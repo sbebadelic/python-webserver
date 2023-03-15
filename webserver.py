e050089f682f3a4e0d8e5281b79c355617a2cd50
Esben Søe <eso@trifork.com>
2023-03-15 20:15:18 +0100

    add webserver ip address to response

--- webserver.py
+++ webserver.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, wafaewfraewfarld!'
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return f"Hello, wafaewfraewfarld! {ip_address}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5001)
